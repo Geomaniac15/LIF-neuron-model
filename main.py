da7a1eaed274262c725152b2b150952c27bc6203
gveal <gveal07@outlook.com>
2026-03-10 19:38:39 +0000

    two neurons added which affect one another

--- main.py
+++ main.py
@@ -9,48 +9,101 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-V_rest = -70 # mV
-V_reset = -80 # mV
-V_threshold = -55 # mV
-tau_m = 20 # ms
-R = 10 # M Ohms
-I = 2 # nA
+class Neuron:
+
+    def __init__(self, V_rest, V_reset, V_threshold, tau_m, R, I, w=0):
+        self.V_rest = V_rest
+        self.V_reset = V_reset
+        self.V_threshold = V_threshold
+        self.tau_m = tau_m
+        self.R = R
+        self.I = I
+        self.weight = w
+        self.dV = None
+    
+    def calculate(self, V, synaptic_input=0):
+        dV = (1 / self.tau_m) * (-(V - self.V_rest) + self.R * (self.I + synaptic_input))
+        return dV
+    
+'''
+V_rest = mV
+V_reset = mV
+V_threshold = mV
+tau_m = ms
+R = M Ohms
+I = nA
+'''
+
 dt = 0.1 # ms
+tau_syn = 5 # ms
+I_syn_B = 0 # starts at zero
 
-spike_times = []
-voltage_history = []
+neuronA = Neuron(-70, -80, -55, 20, 10, 2, w=2)
+neuronB = Neuron(-70, -80, -55, 20, 10, 1.4)
 
-def calculate(V, V_rest, R, I):
-    dV = (1 / 20) * (-(V - V_rest) + R * I)
-    return dV
+neuronA_spike_times = []
+neuronB_spike_times = []
 
-V = V_rest
+neuronA_voltage_history = []
+neuronB_voltage_history = []
 
-print(V)
+V_A = neuronA.V_rest
+V_B = neuronB.V_rest
+
+# print(V_A)
 
 for x in range(1_000):
-    dV = calculate(V, V_rest, R, I)
+    dV_A = neuronA.calculate(V_A)
 
-    V = V + dV * dt
-    voltage_history.append(V)
+    V_A = V_A + dV_A * dt
+    neuronA_voltage_history.append(V_A)
 
-    if V >= V_threshold:
-        print(f'spike at step {x}')
-        spike_times.append(x * dt)
-        V = V_reset
+    a_spiked = V_A >= neuronA.V_threshold
+    b_spiked = V_B >= neuronB.V_threshold
+
+    synaptic_input = neuronA.weight if a_spiked else 0
+    dV_B = neuronB.calculate(V_B, I_syn_B)
+
+    V_B = V_B + dV_B * dt
+    neuronB_voltage_history.append(V_B)
+
+    if a_spiked:
+        I_syn_B += neuronA.weight
+        print(f'neuron A spike at step {x}')
+        neuronA_spike_times.append(x * dt)
+
+        V_A = neuronA.V_reset
     
-    print(V)
+    I_syn_B -= (I_syn_B / tau_syn) * dt # decay every step
 
-print(f'total spikes: {len(spike_times)}')
-print(f'average ISI: {(spike_times[-1] - spike_times[0]) / (len(spike_times) - 1):.2f} ms')
+    if b_spiked:
+        print(f'neuron B spike at step {x}')
+        neuronB_spike_times.append(x * dt)
 
-steps = np.arange(len(voltage_history)) * dt
+        V_B = neuronB.V_reset
+    
+    # print(f'V_A: {V_A}')
+    # print(f'V_B: {V_B}')
+
+# print(f'total spikes: {len(spike_times)}')
+# print(f'average ISI: {(spike_times[-1] - spike_times[0]) / (len(spike_times) - 1):.2f} ms')
+
+A_steps = np.arange(len(neuronA_voltage_history)) * dt
+B_steps = np.arange(len(neuronB_voltage_history)) * dt
 
 plt.figure(figsize=(12, 4))
-plt.plot(steps, voltage_history, color='teal')
-plt.axhline(V_threshold, color='orange', linestyle='--', label='threshold')
-plt.axhline(V_rest, color='steelblue', linestyle='--', label='rest')
+
+plt.plot(A_steps, neuronA_voltage_history, color='teal')
+plt.plot(B_steps, neuronB_voltage_history, color='red')
+
+plt.axhline(neuronA.V_threshold, color='orange', linestyle='--', label='A threshold')
+plt.axhline(neuronA.V_rest, color='steelblue', linestyle='--', label='A rest')
+
+plt.axhline(neuronB.V_threshold, color='purple', linestyle='--', label='B threshold')
+plt.axhline(neuronB.V_rest, color='gray', linestyle='--', label='B rest')
+
 plt.xlabel('time (ms)')
 plt.ylabel('membrane voltage (mV)')
+
 plt.legend()
 plt.show()
