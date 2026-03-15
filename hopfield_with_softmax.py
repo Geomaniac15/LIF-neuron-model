59804ed81253dd39d8e853dd25608ec8aa47ecf5
gveal <gveal07@outlook.com>
2026-03-15 21:32:11 +0000

    gonna train hopfield on mnist

--- hopfield_with_softmax.py
+++ hopfield_with_softmax.py
@@ -1,5 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+
+mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+X = mnist.data
+y = mnist.target.astype(int)
+
+X_binary = np.where(X > 127, 1.0, -1.0)
+print(X_binary)
 
 def energy(state, W):
     return -0.5 * state @ W @ state
