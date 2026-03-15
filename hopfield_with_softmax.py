aaa6f630ac4aefe1a3a8e3b90af787eb564e9e9c
gveal <gveal07@outlook.com>
2026-03-15 21:27:29 +0000

    another commit aha

--- hopfield_with_softmax.py
+++ hopfield_with_softmax.py
@@ -49,7 +49,8 @@ n_trials = 100
 
 patterns = np.array([np.random.choice([-1, 1], size=N) for _ in range(100)])
 
-for beta in [0.1, 0.5, 1.0, 2.0, 5.0]:
+for d in [4, 8, 16, 32, 64, 100]:
+    beta = 1 / np.sqrt(d)
     successes = 0
     for trial in range(20):
         state = patterns[0].astype(float).copy()
@@ -66,4 +67,4 @@ for beta in [0.1, 0.5, 1.0, 2.0, 5.0]:
         if np.array_equal(retrieved, patterns[0]):
             successes += 1
 
-    print(f'beta={beta}: {successes}/20 correct')
+    print(f'd={d}, beta={beta:.3f}: {successes}/20 correct')
